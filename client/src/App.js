61b3b9f71582b7f7582b03578f0c6b9c4f732f37
skdksldk <tnwns1485@naver.com>
2021-10-14 20:53:36 +0900

    Homepage,Header,Loginpage 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header />
+        <main>
+          <Route exact path='/' component={HomePage} />
+           <Route exact path='/login' component={LoginPage} />
+        </main>
+
+      </Router>
     </div>
   );
 }
