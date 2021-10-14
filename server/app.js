04652a1f3528e80f2a229581f7d7f78f25d5c172
skdksldk <tnwns1485@naver.com>
2021-10-14 12:48:56 +0900

    install react

--- server/app.js
+++ server/app.js
@@ -5,18 +5,7 @@ import morgan from 'morgan';
 import connectDB from './config/db.js';
 
 
-/*
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-
-*/
 var app = express();
 dotenv.config();
 
@@ -24,20 +13,7 @@ connectDB();
 
 
 
-/*
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,4 +31,10 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-//module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(`Server running on port ${PORT}`)
+    //console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
