081b9bc6ce1778ccf1e8ac62e56aad4b1fe2a924
skdksldk <tnwns1485@naver.com>
2021-10-15 21:36:54 +0900

    product라우터 추가

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,16 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import {
-  userLoginReducer
+  userLoginReducer,
+  userRegisterReducer
 } from './reducers/userReducers';
+
+import { logger } from 'redux-logger';
 
 
 const reducer = combineReducers({
-  userLogin: userLoginReducer
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer
 });
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
@@ -20,12 +24,12 @@ const initialState = {
   userLogin: { userInfo: userInfoFromStorage }
 };
 
-const middleware = [thunk];
+//const middleware = [thunk];
 
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
 export default store;
--- server/config/db.js
+++ server/config/db.js
@@ -4,19 +4,19 @@ import mongoose from 'mongoose'
 import dotenv from "dotenv";
 dotenv.config();
 
-const connectDB = async() => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true
-        })
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    })
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.error(`Error: ${error.message}`)
-        process.exit(1)
-    }
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
+  }
 }
 
 export default connectDB
