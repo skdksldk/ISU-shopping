20e104e919b1554a1a4384175ece2a9239992e40
skdksldk <tnwns1485@naver.com>
2022-02-16 12:50:59 +0900

    Update CartPage styling

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -42,6 +42,13 @@ const CartPage = ({ match, location, history }) => {
       ) : (
         <div className='cart__container'>
         <table>
+            <colgroup>
+              <col width="20%" />
+              <col width="40%" />
+              <col width="20%" />
+              <col width="10%" />
+              <col width="10%" />
+            </colgroup>
               <thead>
                   <tr align="center" >
                       <th>이미지</th>
@@ -53,12 +60,14 @@ const CartPage = ({ match, location, history }) => {
               </thead>
               <tbody>
           {cartItems.map(item => (
-          <tr align="center" >
-                <td><img src={item.image} alt={item.name} /></td>
-                <td><Link to={`/shop/product/${item.product}`}>{item.name}</Link></td>
-                <td>{item.price}</td>
-                <td>
-                <select
+              <tr align="center" >
+              <td>
+                <img className='cart__container__img' src={item.image} alt={item.name} /></td>
+              <td className='cart__container__content'>
+                <Link to={`/shop/product/${item.product}`}>{item.name}</Link></td>
+              <td>{item.price}원</td>
+              <td>
+              <select
                   className='select'
                   value={item.qty}
                   onChange={e =>
@@ -85,22 +94,34 @@ const CartPage = ({ match, location, history }) => {
           </tbody>
           </table>
           <div className='cart__content'>
-            <p>
-              Order quantity:{' '}
-              {cartItems.reduce((acc, item) => acc + item.qty, 0)} Items
-            </p>
-            <p>Delivery: FREE</p>
-            <p className='cart__tot'>
-              TOTAL: $
-              {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)}
-            </p>
-            <button
-              className={`${cartItems.length === 0 ? 'disabled' : 'btn'}`}
-              onClick={checkoutHandler}
-            >
-              PROCEED TO CHECKOUT
-            </button>
+          <table>
+              <colgroup>
+                <col width="20%" />
+                <col width="20%" />
+                <col width="60%" />
+              </colgroup>
+              <thead>
+                <tr align="center" >
+                  <th>총 상품금액</th>
+                  <th>배송비</th>
+                  <th>총 결재금액</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr align="center" >
+                  <td>{cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)}원</td>
+                  <td>0원</td>
+                  <td>={cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)}원</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+          <button
+            className={`${cartItems.length === 0 ? 'disabled' : 'btn'}`}
+            onClick={checkoutHandler}
+          >
+            주문하기
+          </button>
         </div>
       )}
     </div>
