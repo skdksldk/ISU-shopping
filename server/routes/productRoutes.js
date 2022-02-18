80af103417903d72f827a004f78078162acfeb43
skdksldk <tnwns1485@naver.com>
2022-02-18 17:53:14 +0900

    Add CurrentProductList Rest api

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,16 +2,18 @@ import express from 'express';
 const router = express.Router();
 import {
   getProductList,
+  getCurrentProductList,
   getProduct,
   deleteProduct,
   createProduct,
   updateProduct,
-  createProductReview,
+  createProductReview
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authHandler.js';
 
 router.route('/').get(getProductList).post(protect, admin, createProduct);
 router.route('/:id/reviews').post(protect, createProductReview);
+router.route('/current').get(getCurrentProductList).post(protect, admin);
 router
   .route('/:id')
   .get(getProduct)
