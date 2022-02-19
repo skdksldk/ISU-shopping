6761ba2e2cfeed42d0719f56c1deabb91aa4969e
skdksldk <tnwns1485@naver.com>
2022-02-19 13:36:46 +0900

    Add Homepage with Current productlist

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,17 @@
 import React, { useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 import Meta from '../components/Meta';
+import { listCurrentProducts } from '../actions/productActions';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 
 
-// register ScrollTrigger
+
 gsap.registerPlugin(ScrollTrigger);
 
 const HomePage = () => {
@@ -16,6 +21,15 @@ const HomePage = () => {
   const refSlide2 = useRef(null);
   
   const revealRef1 = useRef(null);
+
+
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const { loading, error, products, page, pages } = productList;
+
+  useEffect(() => {
+    dispatch(listCurrentProducts());
+  }, [dispatch]);
   
 
   useEffect(() => {
@@ -80,9 +94,8 @@ const HomePage = () => {
       <section className='section' ref={refSlide1}>
       <div className='section__image section__image--intro1'></div>
         <div className='section__content'>
-          <h1 className='section__content__title'>INVITE YOU</h1>
           <h3>
-            Explore this week's latest collection of the season curated for you
+            Explore this week's latest collection 
           </h3>
           <Link to='/login'>LOGIN</Link>
         </div>
@@ -91,11 +104,21 @@ const HomePage = () => {
       <section className='section' ref={refSlide2}>
         <div className='section__image section__image--intro2'></div>
         <div className='section__content' ref={revealRef1}>
-          <h1 className='section__content__title'>WELCOME</h1>
-          <h3>
-            Explore this week's latest collection of the season curated for you
-          </h3>
-          <Link to='/login'>LOGIN</Link>
+        <div className='intro__container'>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <div className='error'>
+              <Message>{error}</Message>
+            </div>
+          ) : (
+            <div className='intro_products'>
+              {products.map(product => (
+                <Product key={product._id} product={product} />
+              ))}
+            </div>
+          )}
+        </div>
         </div>
       </section>
 
